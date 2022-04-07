3cb3d859b8548c6b61a593d8f8a2fbc1430a5dfa
Lauren McCall <lmmccolloch@gmail.com>
2022-04-07 12:33:07 -0400

    added snow falling css and snow component

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+## Assignment:
+
+Build a reverse Hangman-style game. The player can't lose; they just get to build a snowman by adding parts each time they guess a letter correctly.
+
+## Credits
+
+snow CSS by Alphardex
+https://codepen.io/alphardex/pen/dyPorwJ
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { Snow } from './components/snow'
+import words from './words.json'
 
 export function App() {
-  return <div>Hello, World </div>
+  console.log(words)
+
+  return (
+    <div>
+      <header>
+        <Snow />
+        <h1>Do You Want To Build A Snowman?</h1>
+      </header>
+    </div>
+  )
 }
